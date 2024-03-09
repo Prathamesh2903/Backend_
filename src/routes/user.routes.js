d63aea486cf2b82a16926bc2b442ade40e666033
Prathamesh Deepak Khanna <prathameshkhanna@gmail.com>
2024-03-09 17:42:07 +0530

    added new user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -26,5 +26,13 @@ userRouter.route("/login").post(loginUser)
 userRouter.route("/logout").post(verifyJWT , logoutUser)
 
 userRouter.route("/refresh-token").post(refreshAccessToken)
+
+userRouter.route("/changePassword").post(verifyJWT,changeCurrentPassword)
+
+userRouter.route("/current-user").get(verifyJWT,getCurrentUser)
+
+userRouter.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+userRouter.route("/history").get(verifyJWT,getWatchHistory)
 
 export default userRouter
